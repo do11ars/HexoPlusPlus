0210bb496c786613c041c6f2d01c2cbfa2f7688d
do11ars <rozibonek1927@gmail.com>
2024-11-12 14:00:42 +0700

    Update tool.js

--- src/tool.js
+++ src/tool.js
@@ -35,7 +35,7 @@ var ajax = ajaxObject();
 };
 
 function hpp_del_all(){
-swal({title:"Do you really want to destroy？",text:"I believe your hand slipped",icon:"warning",buttons:["No", "Yes！"],dangerMode: true}).then((value) => {if(value){swal({title:"Do you really want to delete the data？",text:"I'm thinking you don't want to configure it again.",icon:"warning",buttons:["I give up", "I'm sure"],dangerMode: true}).then((value) => {if(value){swal({title:"Do you really want to start over？",text:"I don't think it's impossible.",icon:"warning",buttons:["I'm wilted", "Death is like home"],dangerMode: true}).then((value) => {if(value){var ajax = ajaxObject();swal({title: "\nDeleting...",icon: "https://cdn.jsdelivr.net/gh/HexoPlusPlus/CDN@db63c79/loading.gif",text:"\n",button: false,closeModal: false,});
+swal({title:"Do you really want to destroy？",text:"I believe your hand slipped",icon:"warning",buttons:["No", "Yes！"],dangerMode: true}).then((value) => {if(value){swal({title:"Do you really want to delete the data?",text:"I'm thinking you don't want to configure it again.",icon:"warning",buttons:["I give up", "I'm sure"],dangerMode: true}).then((value) => {if(value){swal({title:"Do you really want to start over?",text:"I don't think it's impossible.",icon:"warning",buttons:["I'm wilted", "Death is like home"],dangerMode: true}).then((value) => {if(value){var ajax = ajaxObject();swal({title: "\nDeleting...",icon: "https://cdn.jsdelivr.net/gh/HexoPlusPlus/CDN@db63c79/loading.gif",text:"\n",button: false,closeModal: false,});
     ajax.open( "get" , '/hpp/admin/api/del_all' , true );
     ajax.setRequestHeader( "Content-Type" , "text/plain" );
     ajax.onreadystatechange = function () {
